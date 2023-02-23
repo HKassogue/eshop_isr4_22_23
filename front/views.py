d942dec6c0728fd018f21854c271c91f44dd29db
Dr Hamidou Kassogué <hamidoukass@gmail.com>
2023-02-23 17:15:42 +0000

    end of feb_16

---
+++ front/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    #return HttpResponse("<h1>Bienvenu sur notre site!</h1>")
+    return render(request, 'front/index.html')
