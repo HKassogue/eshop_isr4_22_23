68317781e15109d95a899acdc2cca282a5276551
Dr Hamidou Kassogué <hamidoukass@gmail.com>
2023-03-09 02:30:07 +0000

    config for jazzmin and admin volt

--- eshop/settings.py
+++ eshop/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
+    #'admin_volt.apps.AdminVoltConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- eshop/urls.py
+++ eshop/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+#from django.urls import include
 from django.conf.urls.static import static
 from django.conf import settings
 from front import views
@@ -27,6 +28,7 @@ urlpatterns = [
     path('detail', views.detail, name='detail'),
     path('checkout', views.checkout, name='checkout'),
     path('contact', views.contact, name='contact'),
+    #path('', include('admin_volt.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
