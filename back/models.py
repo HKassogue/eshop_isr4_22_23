528a7e81e136aa730d7120213b17b0dc10661e96
Dr Hamidou Kassogué <hamidoukass@gmail.com>
2023-03-09 01:29:50 +0000

    Add of model product with 4 fields

--- back/models.py
+++ back/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=30, unique=True, null=False, blank=False)
+    slug = models.CharField(max_length=50, unique=True, null=False, blank=False)
+    description = models.TextField(max_length=255, null=True, blank=False)
+    details = models.TextField(max_length=255, null=True, blank=False)
+    price = models.FloatField(null=False, blank=False)
