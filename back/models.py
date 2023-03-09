8bbde4c0dbbc8d2e7332af65e1dde78a3933fed5
Dr Hamidou Kassogué <hamidoukass@gmail.com>
2023-03-09 01:31:22 +0000

    Update for description and details for product

--- back/models.py
+++ back/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=30, unique=True, null=False, blank=False)
     slug = models.CharField(max_length=50, unique=True, null=False, blank=False)
-    description = models.TextField(max_length=255, null=True, blank=False)
-    details = models.TextField(max_length=255, null=True, blank=False)
     price = models.FloatField(null=False, blank=False)
+    description = models.TextField(max_length=255, null=True, blank=True)
+    details = models.TextField(max_length=255, null=True, blank=True)
+    
